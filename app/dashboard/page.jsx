689f9ddc9e5e92314d3db6a2c34785d5ff4d7d2d
YiYing Ko <ykocuriosity@gmail.com>
2024-01-02 23:01:27 +0000

    fix heading of scrollbar

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,20 +7,24 @@ export default function Walks() {
   return (
     <div className="dash-wrapper">
       <main className="dash-container">
-        <div className="dash-list">
+        <div>
           <div className="dash-heading">
             <h2>Currently booked walks.</h2>
           </div>
-          <Suspense fallback={<Loading />}></Suspense>
-          <WalkList />
+          <div className="dash-list">
+            <Suspense fallback={<Loading />}></Suspense>
+            <WalkList />
+          </div>
         </div>
 
-        <div className="dash-list">
+        <div>
           <div className="dash-heading">
             <h2>Check Past walk record</h2>
           </div>
-          <Suspense fallback={<Loading />}></Suspense>
-          <PastList />
+          <div className="dash-list">
+            <Suspense fallback={<Loading />}></Suspense>
+            <PastList />
+          </div>
         </div>
       </main>
     </div>
