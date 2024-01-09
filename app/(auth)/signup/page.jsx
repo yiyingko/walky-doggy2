35308d8e05130ae3eefafd2da6c885d39014fc04
YiYing Ko <ykocuriosity@gmail.com>
2024-01-09 22:52:50 +0000

    fix env location & url

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,56 +1,40 @@
 'use client';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
-// components
 import AuthForm from '../AuthForm';
 
 export default function Signup() {
   const router = useRouter();
-  // const [error, setError] = useState('');
+  const [formError, setFormError] = useState('');
 
-  // const handleSubmit = async (e, email, password) => {
-  //   e.preventDefault();
-  //   setError('');
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
 
-  //   const supabase = createClientComponentClient();
-
-  //   const { error } = await supabase.auth.signUp({
-  //     email,
-  //     password,
-  //     options: {
-  //       emailRedirectTo: `${location.origin}/api/auth/callback`,
-  //     },
-  //   });
-  //   if (error) {
-  //     setError(error.message);
-  //   }
-  //   if (!error) {
-  //     router.push('/verify');
-  //   }
-  // };
-
-  const supabase = createClientComponentClient();
-
-  const handleSubmit = async () => {
-    await supabase.auth.signUp({
-      email: 'c5170182@gmail.com',
-      password: '12345',
+    console.log(email, password);
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
       options: {
-        emailRedirectTo: `${location.origin}/auth/callback`,
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
       },
     });
-    //router.refresh();
-    router.push('/verify');
+    if (error) {
+      setFormError(error.message);
+    }
+    if (!error) {
+      router.push('/verify');
+    }
   };
 
   return (
     <main className="auth-wraper">
       <div className="auth-container">
-        <h2>Sign up</h2>
+        <h2>Login</h2>
         <AuthForm handleSubmit={handleSubmit} />
-        {/* {error && <div>{error}</div>} */}
+        {formError && <div>{formError}</div>}
       </div>
     </main>
   );
