f7f8f17d9a40a11368e5dc7a6a80cd6735893af0
YiYing Ko <ykocuriosity@gmail.com>
2024-01-11 16:27:10 +0000

    protect routes from user sign in/ sign out

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,16 @@
 import Link from 'next/link';
 import Navbar from '@/app/components/Navbar-auth';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+
+  if (data.session) {
+    redirect('/dashboard');
+  }
   return (
     <>
       <Navbar />
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,5 +1,6 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 // components
 import Navbar from '@/app/components/Navbar-user';
@@ -7,6 +8,9 @@ import Navbar from '@/app/components/Navbar-user';
 export default async function DashboardLayout({ children }) {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
+  if (!data.session) {
+    redirect('/login');
+  }
 
   return (
     <>
